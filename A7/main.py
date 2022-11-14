36a283369438936f3e13a6ed70c2d482f57feb39
Codrin <codrin.haratu@gmail.com>
2022-11-14 09:49:54 +0200

    Implemented with list and dictionary, but the alternating sequence not yet

--- A7/main.py
+++ A7/main.py
@@ -1,28 +1,68 @@
 from numpy import Infinity
-from ComplexNumber import *
+from math import sqrt as racine
+import copy
 
 def read_number():
     print("Enter a complex number:")
     real = int(input("Real part = "))
     im = int(input("Imaginary part = "))
-    return ComplexNumber(real, im)
+    return real, im
 
-def increasing_modulus_sequence(complex_numbers):
+def print_list(list):
+    for i in range(0, len(list[0])):
+        print(str(list[0][i]) + " + (" + str(list[1][i]) + ")i")
+
+def print_dict(dict):
+    for im in dict.keys():
+        print(str(im) + " + (" + str(dict[im]) + ")i")
+        
+
+def increasing_modulus_list(list):
     sequences = [] # tous les sequences 
-    sequence = [] # temporaire
+    sequence = [[], []] # temporaire
     lowest_modulus = -1
-    for complex_number in complex_numbers:
-        current_modulus = complex_number.modulus()
+    for i in range(0, len(list[0])):
+        a = list[0][i]
+        b = list[1][i]
+        current_modulus = racine(a**2 + b**2)
         if current_modulus > lowest_modulus:
-            sequence.append(complex_number)
+            sequence[0].append(a)
+            sequence[1].append(b)
         else:
-            sequences.append(sequence)
-            sequence.clear()
-            sequence.append(complex_number)
+            sequences.append(copy.deepcopy(sequence))
+            sequence[0].clear()
+            sequence[1].clear()
+            sequence[0].append(a)
+            sequence[1].append(b)
         lowest_modulus = current_modulus
     
     longueur = 0
-    sequence = []
+    sequence = [[], []]
+    for seq in sequences:
+        if len(seq[0]) > longueur:
+            longueur = len(seq[0])
+            sequence = seq
+    
+    return longueur, sequence
+
+def increasing_modulus_dict(dict):
+    sequences = [] # tous les sequences 
+    sequence = {} # temporaire
+    lowest_modulus = -1
+    for im in dict.keys():
+        a = im
+        b = dict[im]
+        current_modulus = racine(a**2 + b**2)
+        if current_modulus > lowest_modulus:
+            sequence.update({a:b})
+        else:
+            sequences.append(copy.deepcopy(sequence))
+            sequence.clear()
+            sequence.update({a:b})
+        lowest_modulus = current_modulus
+    
+    longueur = 0
+    sequence = {}
     for seq in sequences:
         if len(seq) > longueur:
             longueur = len(seq)
@@ -91,26 +131,19 @@ def alternating_real_sequence(complex_numbers):
 
     return longueur, sequence
 
-
-
-
-
-
-
             
 if __name__ == "__main__":
 
-    complex_numbers = []
-    complex_numbers.append(ComplexNumber(2, -1))
-    complex_numbers.append(ComplexNumber(-5, 3))
-    complex_numbers.append(ComplexNumber(1, 1))
-    complex_numbers.append(ComplexNumber(-7, 2))
-    complex_numbers.append(ComplexNumber(-3, 13))
-    complex_numbers.append(ComplexNumber(4, 4))
-    complex_numbers.append(ComplexNumber(-1, -1))
-    complex_numbers.append(ComplexNumber(6, -5))
-    complex_numbers.append(ComplexNumber(-3, -10))
-    complex_numbers.append(ComplexNumber(10, -10))
+    choix = input("What data type are we using?\n 1) List\n 2) Dictionary\n")
+    choix = choix.strip()
+    complex_data = None
+    if choix == "1":
+        complex_data = [[1, 2, -3, 4, -5, -6, 0, 11, 23, 10],
+                        [1, 2, -3, 4, 5, 6, 60, 7, 9, 10]]  
+    else:
+        complex_data = {
+            3:-3, 1:1, -2:-1, 3:12, 4:2, -5:-2, 0:60, 11:7, 23:9, 10:10
+        }
 
     while True:
         print("##### Complex Numbers ####")
@@ -123,30 +156,38 @@ if __name__ == "__main__":
 
         choice = input("Enter Choice: ")
         choice = choice.strip() # pour ignorer les espaces
-        
-        
 
         if choice == "1":
-            new_number = read_number()
-            complex_numbers.append(new_number)
-        elif choice == "2":
-            if len(complex_numbers) == 0:
-                print("The list is empty!")
+            real, im = read_number()
+            if choix == "1":
+                complex_data[0].append(real)
+                complex_data[1].append(im)
             else:
-                for complex_number in complex_numbers:
-                    print(complex_number)
+                complex_data.update({real:im})
+        elif choice == "2":
+            if len(complex_data) == 0:
+                print("The structure is empty!")
+            else:
+                if choix == "1":
+                    print_list(complex_data)
+                else:
+                    print_dict(complex_data)
         elif choice == "3":
-            complex_numbers.clear()
+            complex_data.clear()
         elif choice == "4":
-            longueur, sequence = increasing_modulus_sequence(complex_numbers)
-            print(longueur)
-            for complex_number in sequence:
-                print(complex_number)
+            if len(complex_data) == 0:
+                print("The structure is empty!")
+            else:
+                if choix == "1":
+                    longueur, sequence = increasing_modulus_list(complex_data)
+                    print(longueur)
+                    print_list(sequence)
+                else:
+                    longueur, sequence = increasing_modulus_dict(complex_data)
+                    print(longueur)
+                    print_dict(sequence)
         elif choice == "5":
-            longueur, sequence = alternating_real_sequence(complex_numbers)
-            print(longueur)
-            for complex_number in sequence:
-                print(complex_number)
+            pass
         elif choice == "6":
             print("Have a nice day!")
             exit(1)
