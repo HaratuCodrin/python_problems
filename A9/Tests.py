47e553369ac4ca61edc74a07ffb418ea130cf8af
Codrin <codrin.haratu@gmail.com>
2022-12-11 19:30:44 +0200

    Done A9 without bonuses

--- A9/Tests.py
+++ A9/Tests.py
@@ -1,1 +1,23 @@
 ## Unit tests
+from Services import *
+from ComplexNumber import ComplexNumber
+from collections import deque
+
+def test_add_element(service):
+    n = len(service.repo.liste)
+    service.add_number(1, 2)
+    assert service.repo.liste[n].real == 1
+    assert service.repo.liste[n].im == 2
+
+if __name__ == "__main__":
+    test_list = [
+        ComplexNumber(1,1),
+        ComplexNumber(0,3)
+    ]
+    moves = deque()
+    
+    service = Services(test_list, moves)
+
+    test_add_element(service)
+    
+    print("All tests passed.")
